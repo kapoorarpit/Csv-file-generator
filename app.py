c91f3b5d5073dfbced94ca54d1fdb5a7cdfeb573
kapoorarpit <kapoorarpit2000@gmail.com>
2021-07-06 18:20:57 +0530

    _

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ import csv
 import requests
 import string
 import random
-from collections import defaultdict
+from json import JSONEncoder
+import json
+
 
 
 S = 6
@@ -17,16 +19,28 @@ print(ran)  # print the random data
 
 app = Flask(__name__)
 
+class list_item:
+  def __init__(self):
+    self.name = ""
+    self.vote = ""  
+    self.rating = ""
+    self.num = ""
+    self.img = ""
 
-dict = defaultdict(list)
+def obj_dict(obj):
+    return obj.__dict__
+
+json_string = ""
+
+
+li = []
+
 def completesearch(search_url):
     URL = search_url
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
     r = requests.get(URL, headers=header)
-    # print(r.content)
 
-    # If this line causes an error, run 'pip install html5lib' or install html5lib
     soup = BeautifulSoup(r.text, features='html.parser')
 
     # --------------------------------------------
@@ -35,22 +49,20 @@ def completesearch(search_url):
     name = soup.select('span.lng_cont_name', {'class': 'cntanr'})
     rating = soup.select('span.green-box', {'class': 'cntanr'})
     num = soup.select('span.mobilesv,i.res_contactic.resultimg', {
-                      'class': 'contact-info'})
+                        'class': 'contact-info'})
     img = soup.select('img.altImgcls', {'class': 'cntanr'})
 
     # --------------------------------------------------
     di = {'icon-acb': '0', 'icon-yz': '1', 'icon-acb': '0', 'icon-wx': '2', 'icon-vu': '3', 'icon-ts': '4', 'icon-rq': '5',
-          'icon-po': '6', 'icon-nm': '7', 'icon-lk': '8', 'icon-ji': '9', 'icon-ba': '-', 'icon-hg': ')', 'icon-fe': '(', 'icon-dc': '+'}
+            'icon-po': '6', 'icon-nm': '7', 'icon-lk': '8', 'icon-ji': '9', 'icon-ba': '-', 'icon-hg': ')', 'icon-fe': '(', 'icon-dc': '+'}
     # -----------------------------------------------
 
-    with open(ran, 'w') as f:
-        write = csv.writer(f)
-        count = 0
+    with open("data.csv", 'w') as f:
+        #write = csv.writer(f)
         for i in name:
-
-            dict[count].append(i.text)
-            count = count + 1
-            #write.writerow(i)
+            temp = list_item()
+            setattr(temp, 'name', i.text)
+            li.append(temp)
 
         count = 0
         even = 0
@@ -63,14 +75,12 @@ def completesearch(search_url):
             temp = i.get_text().strip()
             q = temp.split(",")
             w = q[0].split("\t")
-            #write.writerow(w[0].split(","))
-            dict[count].append(w[0].split(","))
+            setattr(li[count],'vote', (w[0].split(","))[0])
             count = count + 1
 
         count = 0
         for i in rating:
-            #write.writerow(i)
-            dict[count].append(i.text)
+            setattr(li[count], 'rating', i.text)
             count = count + 1
 
         count = 0
@@ -81,25 +91,31 @@ def completesearch(search_url):
                     q = str.split(",")
                     if q==[""]:
                         continue
-                    dict[count].append(q)
+                    setattr(li[count], 'num', q[0])
                     count = count + 1
-                    #write.writerow(q)
                     str = ""
                     continue
                 if len(i['class']) != 0:
                     q = (di[i['class'][1]])
                     str = str+q
+        q = str.split(",")
+        setattr(li[count], 'num', q[0])
 
         count = 0
         for i in img:
             temp = i['data-src']
             temp1 = temp.split(",")
-            dict[count].append(temp1)
+            setattr(li[count], 'img', temp1[0])
             count = count + 1
 
-            #write.writerow(temp1)
-        for i in dict:
-            write.writerow(dict[i])
+        '''for i in li:
+            print(i.name, i.vote, i.rating, i.num, i.img)'''
+ 
+        global json_string 
+        json_string = json.dumps(li, default=obj_dict)
+        print(json_string)
+        #print(json_string)
+
 
 
 # home page---------------
@@ -114,9 +130,10 @@ def inde():
     return render_template('home.html')
 
 
+
 @app.route('/alt/')
 def na():
-    return jsonify(dict)
+    return json_string
 
 
 @app.route('/download')
@@ -125,16 +142,6 @@ def download_file():
     return send_file(p, as_attachment=True)
 
 
-'''
-@app.route('/search/')
-def searchitem()
-    return "task done"
-'''
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-'''
-the csv file can be rendered at the next page and will give a download link
-search item will do all the task and take url and make csv file
-'''
