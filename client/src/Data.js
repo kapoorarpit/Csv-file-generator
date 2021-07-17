8208ea192c1b346205ea32c02ac6c2bd6f0ae0c4
kapoorarpit <kapoorarpit2000@gmail.com>
2021-07-17 20:12:03 +0530

    _

--- client/src/Data.js
+++ client/src/Data.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Header} from 'semantic-ui-react';
+import { Header, ItemHeader} from 'semantic-ui-react';
 import { List } from 'semantic-ui-react';
 
-export const Data = ({Data})=>{
+export const Data = ({data})=>{
     return (
         <List>
-            {Data.map(item=>{
+            {data.map(item=>{
                 return(
                     <List.Item key={item.name}>
                         <Header>
                             {item.name}
                         </Header>
+                        <li>{item.vote}</li>
+                        <li>{item.ratig}</li>
+                        <li>{item.num}</li>
+                        <li>{item.img}</li>
                     </List.Item>
                 );
             })}
